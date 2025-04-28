676b86e14ddd6bd0671b6c18f89876c92b9c4f20
Sahil Joshi <155978934+sahiljoshi24@users.noreply.github.com>
2025-04-28 20:52:25 +0530

    Update TokenRing.java

--- Assignment_5/TokenRing.java
+++ Assignment_5/TokenRing.java
@@ -2,54 +2,41 @@ import java.util.Scanner;
 
 class TokenRing 
 {
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) throws Throwable 
+    {    
+        System.out.println("Enter the number of processes: ");
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the number of processes: ");
+        System.out.print("Enter n : ");
         int n = sc.nextInt();
         int token = 0;
-
         int sender, receiver;
         String message;
-
-        System.out.println("\nInitializing Ring:");
-        for (int i = 0; i < n; i++) 
+        System.out.println("Initializing ring");
+        for(int i = 0; i < n; i++) 
         {
-            System.out.print(" " + i);
+            System.out.print(" " + i);                        
         }
-
-        System.out.print("\n\nEnter sender: ");
+        System.out.print("\nEnter sender : ");
         sender = sc.nextInt();
-
-        System.out.print("Enter receiver: ");
+        System.out.print("Enter receiver : ");
         receiver = sc.nextInt();
         sc.nextLine();
-
-        System.out.print("Enter message: ");
+        System.out.print("Enter message : ");
         message = sc.nextLine();
 
-        System.out.println("\nToken Circulation Started:");
-        int i = token;
-        while (i != sender) 
+        System.out.println("Token Cicrulation Started");
+        for(int i = token; i != sender; i++) 
         {
-            System.out.print(" " + i + " ->");
-            i = (i + 1) % n;
+            System.out.print(" " + i + "->");
         }
         System.out.println(" " + sender);
-
-        System.out.println(sender + " Sending Message: '" + message + "'");
-
-        i = (sender + 1) % n;
-        while (i != receiver) 
+        System.out.println(sender + " Sending Message : " + message);
+        
+        for(int i = sender + 1; i != receiver; i = (i+1) % n)
         {
             System.out.println("Message '" + message + "' forwarded by " + i);
-            i = (i + 1) % n;
         }
-
-        System.out.println("Receiver " + receiver + " Received Message: '" + message + "'");
+        System.out.println("Receiver " + receiver + " Received Message: " + message);
         token = sender;
-
-        sc.close();
     }
 }
